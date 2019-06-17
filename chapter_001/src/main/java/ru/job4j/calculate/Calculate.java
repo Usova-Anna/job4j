5e0b08eb941d837ce645624ef52f8b6619c0fdf4
Anna Usova <usova.anna@gmail.com>
2019-06-17 20:59:37 +0300

    1.3 JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,4 +1,17 @@
+/** Это класс Calculate,
+* он будет что-то считать, 
+*скорее всего неправильно.
+*@author Anna Usova
+*@since 17th June 2019
+*@version 1.0
+*/
 public class Calculate{
+/**Это главный метод.
+*Он принимает параметры:
+*@param String[] args
+*@return ничего не возвращает, 
+* это ж void.
+*/
 public static void main (String[] args){
 System.out.println("Hello there!");
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+*Package for calculate task.
+*
+*@author Anna Usova
+*@version $Id$
+*@since 17th June 2019
+*/
+package ru.job4j.calculate;
