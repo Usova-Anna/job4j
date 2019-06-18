c2fdc3caef468cf61d649ad348a45e292fc03ed9
Anna Usova <usova.anna@gmail.com>
2019-06-18 19:27:07 +0300

    1.4 Junit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,18 +1,51 @@
 package ru.job4j.calculate;
 
+
 /** Это класс Calculate,
+
 * он будет что-то считать, 
+
 *скорее всего неправильно.
+
 *@author Anna Usova
+
 *@since 17th June 2019
+
 *@version 1.0
+
 */
+
+
 public class Calculate{
+
 /**Это главный метод.
+
 *Он принимает параметры:
+
 *@param args аргументы
+
 */
+
 public static void main (String[] args){
+
+    
 System.out.println("Hello there!");
+
 }
+    
+/**
+     
+* Method echo.
+     
+* @param name Your name.
+     
+* @return Echo plus your name.
+     
+*/
+    
+
+public String echo(String name) {
+        
+return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.calculate;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**???? ???
+*
+*@author ????
+*@since 18th June 2019
+*@version 1.0
+*/
+
+public class CalculateTest { 
+@Test
+	public void echoName() {
+	String input = "Petr";
+	String expect = "Echo, echo, echo : Petr";
+	Calculate calc = new Calculate();
+	String result = calc.echo(input);
+	assertThat(result,is(expect));
+	}
+}
+
+
