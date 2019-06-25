4afdb5a61d37954e5f054bda518f71c5ff0a6a05
Anna Usova <usova.anna@gmail.com>
2019-06-25 16:45:36 +0300

    4.2. Максимум из двух чисел[#147877]. Убрана избыточная переменная max

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
 public class Max {
     public int max(int first, int second) {
 
-        int max = first > second ? first : second;
-        return max;
+       return first > second ? first : second;
+
 
     }
 }
