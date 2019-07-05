5b0ceb08076db03bb12525debe56f7684bd38353
Anna Usova <usova.anna@gmail.com>
2019-07-05 14:50:00 +0300

    6.3. Массив заполнен true или false[#147895]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -1,4 +1,21 @@
 package ru.job4j.array;
 
 public class Check {
+    /**Метод проверяет, что все элементы в массиве одинаковые.
+     *
+     * @param data массив с true и false.
+     * @return true - все элементы одинаковые (true или false). False - элементы разные.
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = false;
+        for (int i = 0; i != data.length - 1; i++) {
+            if (data[i] != data[i + 1]) {
+                break;
+            }
+            if (i == data.length - 2) {
+                result = true;
+            }
+        }
+        return result;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/CheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -1,4 +1,24 @@
 package ru.job4j.array;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class CheckTest {
+    @Test
+    public void whenNumberOfElementsIsEven() {
+        Check check = new Check();
+        boolean[] checkedArray = {false, false, false, false};
+        boolean testedResult = check.mono(checkedArray);
+        boolean expectedResult = true;
+        assertThat(testedResult, is(expectedResult));
+    }
+
+    @Test
+    public void whenNumberOfElementsIsOdd() {
+        Check check = new Check();
+        boolean[] checkedArray = {false, false, false};
+        boolean testedResult = check.mono(checkedArray);
+        boolean expectedResult = true;
+        assertThat(testedResult, is(expectedResult));
+    }
 }
