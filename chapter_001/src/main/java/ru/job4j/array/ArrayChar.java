c13510392b2b9e927de3931acdc9c90236295539
Anna Usova <usova.anna@gmail.com>
2019-07-06 18:24:32 +0300

    6.4. Слово начинается с ...[#147894]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,4 +1,16 @@
 package ru.job4j.array;
 
+/**@author Anna
+ * @since 05 July 2019
+ */
 public class ArrayChar {
+    /**метод проверяет, что слово начинается c определенной последовательности.
+     *
+     * @param word проверяемая строка.
+     * @param prefix искомое начало строки.
+     * @return true(найдено в начале) или false (не найдено).
+     */
+    public boolean startsW(String word, String prefix) {
+        return word.startsWith(prefix);
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -1,4 +1,25 @@
 package ru.job4j.array;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class ArrayCharTest {
+    @Test
+    public void whenStartsWithPrefix() {
+        ArrayChar line = new ArrayChar();
+        String checkedLine = "Хорошее начало";
+        String checkedPrefix = "Хорош";
+        boolean testedResult = line.startsW(checkedLine, checkedPrefix);
+        assertThat(testedResult, is(true));
+    }
+
+    @Test
+    public void whenDoesNotStartWithPrefix() {
+        ArrayChar line = new ArrayChar();
+        String checkedLine = "Хорошее начало";
+        String checkedPrefix = "хорош";
+        boolean testedResult = line.startsW(checkedLine, checkedPrefix);
+        assertThat(testedResult, is(false));
+    }
+
 }
