49f67bda83434377d1b2896e76e525a31a8f4339
Anna Usova <usova.anna@gmail.com>
2019-07-19 00:31:01 +0300

    Экзамен. Базовый синтаксис.[#147926]

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -16,26 +16,22 @@ public class Merge {
         int[] merged = new int[sizeOfMerged];
         int startPoint1 = 0, startPoint2 = 0; //Начальные точки для прохода по массивам
         int recordIndex = 0; //Ячейка записи в объединённом массиве
-        int min1=left[0], min2=right[0]; //Минимумы в двух массивах
-        int indexMin1=0, indexMin2=0; //индексы минимумов в двух массивах
         while (recordIndex < sizeOfMerged) {
-            for (int i=startPoint1; i < left.length; i++) { //ищем мин в левом массиве
-                indexMin1 = findMinindex(left,startPoint1);
-                min1 = left[indexMin1];
-            }
-            for (int i=startPoint2; i < right.length; i++) { //ищем мин в правом массиве тем же методом
-                indexMin2 = findMinindex(right, startPoint2);
-                min2 = right[indexMin2];
-            }
-            if (min1 < min2) { //записываем в объединённый массив либо минимум из левого массива, либо из правого
-                startPoint1--;
-                merged[recordIndex] = left[indexMin1];
-            } else {
-                startPoint2--;
-                merged[recordIndex] = right[indexMin2];
+
+                if (startPoint1 >= left.length) {   //Если левый массив закончился
+                    merged[recordIndex] = right[startPoint2];
+                    startPoint2++;
+                } else if (startPoint2 >= right.length) { //...или правый закончился.
+                    merged[recordIndex] = left[startPoint1];
+                    startPoint1++;
+                } else if (left[startPoint1] < right[startPoint2]) { //Если массив не закончился и текущий элемент левого меньше правого
+                merged[recordIndex] = left[startPoint1];
+                startPoint1++;
+            } else if (right[startPoint2] <= left[startPoint1]) { //Если массив не закончился и текущий элемент левого не меньше правого
+                merged[recordIndex] = right[startPoint2];
+                startPoint2++;
             }
             recordIndex++;
-
         }
 return merged;
     }
--- chapter_001/src/test/java/ru/job4j/array/MergeTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MergeTest.java
@@ -6,13 +6,22 @@ import static org.hamcrest.core.Is.is;
 public class MergeTest {
     @Test
     public void whenLeftAndRightAreMerged() {
-        int[] testedArray1 = {-20,-18,-6,0,1};
-        int[] testedArray2 = {-19, -18, 0, 2};
-        int[] expectedResult = {-20,-19,-18,-6,0,0,1,2};
+        int[] testedArray1 = {-20, -18, -6, 0,1};
+        int[] testedArray2 = {-18,1,2};
+        int[] expectedResult = {-20,-18,-18,-6,0,1,1,2};
         Merge midget = new Merge();
         int[] testedResult = midget.merge(testedArray1, testedArray2);
         assertThat (testedResult, is(expectedResult));
 
     }
+    @Test
+    public void whenLeftAndRightLongAreMerged() {
+        int[] testedArray1 = {-18,1,2};
+        int[] testedArray2 = {-20, -18, -6, 0,1};
+        int[] expectedResult = {-20,-18,-18,-6,0,1,1,2};
+        Merge midget = new Merge();
+        int[] testedResult = midget.merge(testedArray1, testedArray2);
+        assertThat (testedResult, is(expectedResult));
 
+    }
 }
