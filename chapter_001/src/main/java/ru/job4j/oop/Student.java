19006a3f776d7af0f506361543ca9ce96dbe9d11
Anna Usova <usova.anna@gmail.com>
2019-08-04 18:55:45 +0300

    Проведён validate проектов.

--- chapter_001/src/main/java/ru/job4j/oop/Student.java
+++ chapter_001/src/main/java/ru/job4j/oop/Student.java
@@ -2,18 +2,17 @@ package ru.job4j.oop;
 
 public class Student {
     public void music(String lyrics) {
-        System.out.println("I can sing a song:"+lyrics);
+        System.out.println("I can sing a song:" + lyrics);
     }
 
     public void song() {
         System.out.println("I believe I can fly");
     }
+
     public static void main(String[] args) {
         Student petya = new Student();
-        String song ="I believe";
+        String song = "I believe";
         petya.music(song);
         petya.song();
-
     }
-
 }
