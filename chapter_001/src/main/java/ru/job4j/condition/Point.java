1a26d695781e4654944a924f4bb77ad0e373d260
Anna Usova <usova.anna@gmail.com>
2019-08-05 14:52:38 +0300

    4. Расстояние между точками в трехмерном пространстве.[#147992]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,47 +1,49 @@
-//package ru.job4j.condition;
-//
-//
-//public class Point {
-//    /**Вычисляет расстояние между точками в системе координат.
-//     *
-//     * @param x1 координата первой точки по оси х.
-//     * @param x2 координата второй точки по оси х.
-//     * @param y1 координата первой точки по оси у.
-//     * @param y2 координата второй точки по оси у.
-//     * @return возвращает длину прямой.
-//     */
-//    /**
-//     * Это поля объекта. Они доступны только конкретному объекту.
-//     */
-//    private int x, y,z;
-//
-//    /**
-//     * Конструктор, который принимает начальное состояние объекта "точка"
-//     *
-//     * @param first  координата x
-//     * @param second координата y
-//     */
-//    public Point(int first, int second) {
-//        this.x = first;
-//        this.y = second;
-//    }
-//
-//    public Point (int first, int second, int third) {
-//        this.x=first;
-//        this.y=second;
-//        this.z=third;
-//    }
-//
-//    public double distance(Point that) {
-//        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y2 - this.y1, 2)); //this.x = поле объекта a. that.x = поле объекта b.
-//        //This - текущий объект
-//    }
-//
-//    public static double distance3d(Point that) {
-//return Math.sqrt(Math.pow(this.x2-this.x1,2)+Math.pow(this.y2-this.y1,2)+Math.pow(this.z2-this.z1,2));
-//    }
-//
-//    public void info() {
-//        System.out.println(String.format("Point [%s, %s]", this.x, this.y));
-//    }
-//}
+package ru.job4j.condition;
+
+public class Point {
+
+
+    /**
+     * Это поля объекта. Они доступны только конкретному объекту.
+     */
+    private int x, y, z;
+
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     *
+     * @param first  координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
+
+    /**Считает расстояние между двумя точками в двухмерном пространстве
+     *
+     * @param that объект точка с координатами в двумерном пространстве. Первая точка берётся из поля this.x, из объекта к которому вызывается метод.
+     * @return расстояние.
+     */
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)); //this.x = поле объекта a. that.x = поле объекта b.
+        //This - текущий объект
+    }
+    /**Считает расстояние между двумя точками в трёхмерном пространстве
+     *
+     * @param that объект точка с координатами в трёъмерном пространстве. Первая точка берётся из поля this.x, из объекта к которому вызывается метод.
+     * @return расстояние.
+     */
+    public double distance3d(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2) + Math.pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point [%s, %s]", this.x, this.y));
+    }
+}
