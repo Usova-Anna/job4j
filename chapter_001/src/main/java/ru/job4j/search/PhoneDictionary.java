8d5f269e28654f6ff0ed3f7a40be60f166076145
Anna Usova <usova.anna@gmail.com>
2020-01-13 23:34:47 +0300

    Телефонный справочник на базе ArrayList[#147942]

--- chapter_001/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_001/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -20,17 +20,9 @@ public class PhoneDictionary {
 
         ArrayList<Person> result = new ArrayList<>();
         for (Person x : persons) {
-            if (x.getName().contains(key)) { //Поиск только по полю Имён
+            if (x.getName().contains(key) || x.getSurname().contains(key) || x.getAddress().contains(key) || x.getPhone().contains(key)) { //Поиск только по полям: имя, фамилия, адрес, номер
                 result.add(x);
-            }
-            if (x.getSurname().contains(key)) { //Поиск только по полю Фамилий
-                result.add(x);
-            }
-            if (x.getAddress().contains(key)) { //Поиск только по полю адресов
-                result.add(x);
-            }
-            if (x.getPhone().contains(key)) { //Поиск по полю номеров
-                result.add(x);
+
             }
 
         }
