59f293febb62af9aa91933dcf7766f815bffa3d5
Anna Usova <usova.anna@gmail.com>
2020-01-24 17:44:06 +0300

    Идентичные тексты. [#212655]

--- chapter_001/src/main/java/ru/job4j/collection/UniqueText.java
+++ chapter_001/src/main/java/ru/job4j/collection/UniqueText.java
@@ -3,13 +3,22 @@ package ru.job4j.collection;
 import java.util.HashSet;
 
 public class UniqueText {
-    public static boolean isEqual (String originText, String duplicateText) {
-boolean rsl = true;
-String[] origin = originText.split(" ");
-String[] text = duplicateText.split(" ");
-HashSet<String> check = new HashSet<>();
-// for-each origin -> new HashSet.
-        // for-each text -> hashSet.contains
+    public static boolean isUnique(String originText, String duplicateText) {
+
+        boolean rsl = false; //тексты одинаковые
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String originWord : origin) {
+            check.add(originWord); //получаем HashSet с уникальными словами текста origin
+        }
+        for (String checkedWord : text) {
+            if (!check.contains(checkedWord)) {//если текст не содержит слово
+                rsl = true; //то текст уникальный
+                break;
+            }
+
+        }
         return rsl;
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/collection/UniqueTextTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UniqueTextTest {
+
+    @Test
+    public void SameTexts() {
+
+        String origin = "My cat eats a mouse";
+        String text = "My cat eats a mouse";
+        assertThat(UniqueText.isUnique(origin, text), is(false));
+    }
+
+    @Test
+    public void isNotEquals() {
+
+        String origin = "My cat eats a mouse";
+        String text = "A mouse is eaten by a cat";
+        assertThat(UniqueText.isUnique(origin, text), is(true));
+    }
+
+    @Test
+    public void ifTextHasTheSameWordsDifferentOrder() {
+
+        String origin = "My cat eats a mouse";
+        String text = "a cat eats My mouse";
+        assertThat(UniqueText.isUnique(origin, text), is(false));
+    }
+}
