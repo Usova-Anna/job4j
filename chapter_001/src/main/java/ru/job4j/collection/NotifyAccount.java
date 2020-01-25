dcfec50636c6d27a8958e646c6386fd7f9238147
Anna Usova <usova.anna@gmail.com>
2020-01-25 17:57:44 +0300

    Модели данных и HashSet. [#218530] Создание уникального списка рассылки

---
+++ chapter_001/src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+/*
+Модели данных и HashSet. [#218530]
+ */
+public class NotifyAccount {
+    public static HashSet<Account> sent(List<Account> accounts) {
+
+        HashSet<Account> rsl = new HashSet<>();
+        // for-each accounts to HashSet;
+        for (Account line : accounts) {
+            rsl.add(line);
+        }
+        return rsl;
+    }
+}
