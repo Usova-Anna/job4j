1fe14a216c563c646e2ea7ebe03d0c49d90e3de8
Anna Usova <usova.anna@gmail.com>
2020-01-27 15:05:58 +0300

    Map, HashMap [#212652]

--- chapter_001/src/main/java/ru/job4j/collection/UsageMap.java
+++ chapter_001/src/main/java/ru/job4j/collection/UsageMap.java
@@ -1,4 +1,18 @@
 package ru.job4j.collection;
 
+import java.util.HashMap;
+
 public class UsageMap {
+    public static void main(String[] args) {
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("arseniy@mail.ru", "Иванов Арсений Евгеньевич");
+        map.put("arseniy@mail.ru", "Иванов Арсений Евгеньевич");//дублируем значение, которое внесётся только единожды
+        map.put("egor@yandex.ru", "Кузнецов Егор Иванович");
+        map.put("emelya@gmail.com", "Васин Емеля Дмитриевич");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println("Ключ " + key + "; значение - " + value);
+        }
+    }
 }
