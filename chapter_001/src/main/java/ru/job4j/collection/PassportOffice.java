aa67ea2c21f408bfdb9db998212701f1abcdedc8
Anna Usova <usova.anna@gmail.com>
2020-01-28 08:33:00 +0300

    212656 Паспорт и жители(половина)

--- chapter_001/src/main/java/ru/job4j/collection/PassportOffice.java
+++ chapter_001/src/main/java/ru/job4j/collection/PassportOffice.java
@@ -1,4 +1,29 @@
 package ru.job4j.collection;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false; //процедура добавления не произведена
+        boolean exists = true; //Такой гражданин уже есть в базе
+        for (String key:citizens.keySet()) {
+            if (!key.equals(citizen.getPassport())) {
+                exists=false; //Такого гражданина нет
+            }
+        }
+        if (!exists) { //Если такого гражданина нет
+            citizens.put(citizen.getPassport(), citizen); //то добавляем его
+            rsl = true;//добавлен
+        }
+        return rsl;
+    }
+
+
+    public Citizen get(String passport) {
+        return null;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -1,4 +1,28 @@
 package ru.job4j.collection;
 
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 public class PassportOfficeTest {
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void addWhenCitizenAlreadyExists () {
+        Citizen man = new Citizen("1236 996655", "Ужесин Есть Присутствуевич");
+
+
+        PassportOffice po = new PassportOffice();
+        po.add(man); //1st adding
+        boolean actual = po.add(man); //2nd adding of the same person
+        boolean expected = false;
+        assertThat(actual,is(expected));
+
+    }
+
 }
