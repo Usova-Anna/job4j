24043150911da18a426bc9437c5148cd89586708
Anna Usova <usova.anna@gmail.com>
2020-02-08 21:11:27 +0300

    UserInputException[#234585]

---
+++ chapter_002/src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,8 @@
+package ru.job4j.ex;
+import org.junit.Test;
+public class CountTest {
+    @Test  (expected = IllegalArgumentException.class)
+    public void whenStartGreaterThanFinish() {
+        Count.add(10,2);
+    }
+}
