02476361292321f9fb11fe6902fc30e78eaf9664
Anna Usova <usova.anna@gmail.com>
2020-02-09 18:33:12 +0300

    Сортировка[#234528] с работающими тестами (сравнение имён объектов заявок)

--- chapter_002/src/main/java/ru/job4j/tracker/ItemSorter.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ItemSorter.java
@@ -7,17 +7,17 @@ public class ItemSorter {
 
     //сортировка по возрастанию
     public List<Item> sortAZ(List<Item> items) {
-        System.out.println("Not sorted:\n" + items + "\n");
+        System.out.println("Not sorted:" + System.lineSeparator() + items + System.lineSeparator());
         Collections.sort(items, new ItemComparator());
-        System.out.println("Sorted using Comparator (ascending):\n" + items + "\n");
+        System.out.println("Sorted using Comparator (ascending):" + System.lineSeparator() + items + System.lineSeparator());
         return items;
     }
 
     //сортировка по убыванию
     public List<Item> sortZA(List<Item> items) {
-        System.out.println("Not sorted:\n" + items + "\n");
+        System.out.println("Not sorted:" + System.lineSeparator() + items + System.lineSeparator());
         Collections.sort(items, new ItemComparatorDescending());
-        System.out.println("Sorted using Comparator (descending):\n" + items);
+        System.out.println("Sorted using Comparator (descending):" + System.lineSeparator() + items);
         return items;
     }
 }
