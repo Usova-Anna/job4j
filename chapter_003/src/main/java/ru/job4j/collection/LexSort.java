7d99bcf2c454eecc28fdb113c1790a94a49696ba
Anna Usova <usova.anna@gmail.com>
2020-03-05 02:08:22 +0300

    Сортировка номера как части строки (с replaceAll).[#234525]

--- chapter_003/src/main/java/ru/job4j/collection/LexSort.java
+++ chapter_003/src/main/java/ru/job4j/collection/LexSort.java
@@ -10,16 +10,9 @@ public class LexSort implements Comparator<String> {
         String[] tempR = right.split(" ");
         String strNumL = tempL[0];
         String strNumR = tempR[0];
-        String[] withoutDotsL = strNumL.split("\\.");
-        String[] withoutDotsR = strNumR.split("\\.");
-        strNumL = "";
-        strNumR = "";
-        for (String x : withoutDotsL) {
-            strNumL = strNumL + x;
-        }
-        for (String x : withoutDotsR) {
-            strNumR = strNumR + x;
-        }
+        strNumL=strNumL.replaceAll("\\.","");
+        strNumR=strNumR.replaceAll("\\.","");
+
         int resultL = Integer.valueOf(strNumL);
         int resultR = Integer.valueOf(strNumR);
         return Integer.compare(resultL, resultR);
